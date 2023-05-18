ca3d93012ae46a71615972adb0daf7baca3deb06
yogeshtakeo <yogesh@takeo.ai>
2023-05-17 22:47:40 -0400

    Added React Vite Project Task 3

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello ! This is me</h1>
+
+      <p>This is the dumbest person in the world , Mr Yogesh Bhattarai.</p>
+    </>
+  );
+}
+
+export default App;
