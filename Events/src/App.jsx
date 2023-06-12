f1e1d605cd836adfd70fb05076f7c757dac9f791
yogeshtakeo <yogesh@takeo.ai>
2023-06-11 22:09:01 -0400

    Added events onChange and onClick

--- Events/src/App.jsx
+++ Events/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contactName, setContactName] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+
+  const handleEvent = (event) => {
+    console.log(event.target);
+    console.log("hey this is an event");
+  };
+
+  const onContactName = (event) => {
+    console.log(event.target.value);
+    setContactName(event.target.value);
+  };
+
+  const onContactNumber = (event) => {
+    console.log(event.target.value);
+    setContactNumber(event.target.value);
+  };
 
   return (
     <>
+      <h1>.{contactName}</h1>
+      <h1>#{contactNumber}</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          onChange={onContactName}
+          placeholder="Contact Name"
+        />
+        <input
+          type="number"
+          placeholder="Contact Number"
+          onChange={onContactNumber}
+        />
+        <input type="file" />
+        <input type="color" />
+
+        <button onClick={handleEvent}>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/*  , kabina , junu ,  */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
